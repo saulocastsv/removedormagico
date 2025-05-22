59f6d895d56e29539d9bb7534c827c6f788bd5ad
saulocastsv <saulitromarketing@gmail.com>
2025-05-22 14:02:27 -0300

    update 2

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], 
+  darkMode: 'class',  // ✅ Já está como sugerimos!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
